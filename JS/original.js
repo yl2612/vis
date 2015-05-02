db7f35b388fc06bdc5da39a0d9bc45c071978b6b
Yunshi <yl2612@nyu.edu>
2015-05-02 15:53:17 -0400

    add

--- JS/original.js
+++ JS/original.js
@@ -53,53 +53,21 @@ var svg = svgContainer
 
 var fileName = "data/test2.csv";
 //var fileName = "data/bar_chart.csv";
-var	parseDate = d3.time.format("%Y-%m-%d").parse;
-//var	parseDate = d3.time.format("%Y-%m").parse;
+//var	parseDate = d3.time.format("%Y-%m-%d").parse;
+var	parseDate = d3.time.format("%Y-%m").parse;
 
 function type(d) {
 	  d.Count = +d.Count;
 	  return d;
 	}
 
-
-var bar_plot = function(file, index) {
+var bar_plot = function(file) {
 	d3.csv(file, type, function(error, data) {
-		//console.log(data);
-		console.log(index);
-		Data = data;
 
-		data = data[index];
-		temp = [];
+		x.domain([0,d3.max(data,function(d){return d.Count;})]);
+		y.domain(data.map(function(d){return d.Topics;}));
 
-		temp.push({Count:data.topic1, Topics:1});
-		temp.push({Count:data.topic2, Topics:2});
-		/*
-		temp.push({Count:4, Topics:data.topic4});
-		temp.push({Count:5, Topics:data.topic5});
-		temp.push({Count:5, Topics:data.topic6});
-		temp.push({Count:6, Topics:data.topic7});
-		temp.push({Count:8, Topics:data.topic8});
-		*/
-
-
-		console.log(temp);
-		//x.domain([0,d3.max(temp,function(d){return d.Count;})]);
-		//y.domain(temp.map(function(d){return d.Topics;}));
-
-		x.domain([0,25]);
-		y.domain([1,2]);
-
-		d3.select("svg").remove();
-		svgContainer = d3.select("#Bar_plot").append("svg")
-				.attr("class","container")
-				.attr("x",800)
-				.attr("y",200)
-				.attr("width",  width + margin.left + margin.right)
-			    .attr("height",  height + margin.top + margin.bottom)
-
-		svg = svgContainer
-		  .append("g")
-		    .attr("transform", "translate(" + 50 + "," + margin.top + ")");
+		console.log(data);
 
 		svg.append("g")
 		    .attr("class", "x axis")
@@ -117,7 +85,7 @@ var bar_plot = function(file, index) {
 		    .call(yAxis)
 
 		svg.selectAll(".bar")
-			.data(temp)
+			.data(data)
 			.enter().append("rect")
 			.attr("class","bar")
 			.attr("x",2)
@@ -126,9 +94,8 @@ var bar_plot = function(file, index) {
 			.attr("height", y.rangeBand())
 
 	});
-	
 };
-bar_plot(fileName, 0);
+bar_plot(fileName);
 
 
 // slider:
@@ -142,19 +109,19 @@ var slider2 = d3.select("#Date_slider").append("div")
 	.attr("class","slider");
 
 // set up an array to hold the months
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 // lets be fancy for the demo and select the current month.
-var months = ["2013/06/24","2013/06/25","2013/06/26"]
-
 var activeMonth = new Date().getMonth();
 
 // console.log(activeMonth);
 
 $(".slider")
-    // activate the slider with options    
+                    
+    // activate the slider with options
     .slider({ 
         min: 0, 
         max: months.length-1, 
-        //value: activeMonth 
+        value: activeMonth 
     })
                     
     // add pips with the labels set to "months"
@@ -164,8 +131,6 @@ $(".slider")
     })
                     
     // and whenever the slider changes, lets echo out the month
-    .on("slide", function(e,ui) {
-    	console.log("You selected " + months[ui.value] + " (" + ui.value + ")");
-    	bar_plot(fileName, ui.value);
+    .on("slidechange", function(e,ui) {
         $("#labels-months-output").text( "You selected " + months[ui.value] + " (" + ui.value + ")");
     });
